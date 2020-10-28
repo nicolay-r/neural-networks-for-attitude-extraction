a4f728f1f966c164cfaff7f3a860f836698f1b1d
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-10-28 23:43:34 +0300

    fixed bug with path inside dir.

--- experiment_io.py
+++ experiment_io.py
@@ -1,9 +1,7 @@
-from os.path import join, dirname
-
 from arekit.contrib.networks.core.io_utils import NetworkIOUtils
 
 
 class CustomNetworkExperimentIO(NetworkIOUtils):
 
     def get_experiment_sources_dir(self):
-        return join(dirname(__file__), u"data/")
+        return u"data/"
