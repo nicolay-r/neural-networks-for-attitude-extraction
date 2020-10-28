cb8e8e9970c6d39d88458e88ba4b6c164e279731
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-10-28 23:52:13 +0300

    Switched to name

--- experiment_io.py
+++ experiment_io.py
@@ -4,4 +4,4 @@ from arekit.contrib.networks.core.io_utils import NetworkIOUtils
 class CustomNetworkExperimentIO(NetworkIOUtils):
 
     def get_experiment_sources_dir(self):
-        return u"data/"
+        return u"data"
