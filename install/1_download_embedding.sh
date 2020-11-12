f4e0808ea699c06a8e3862adc3f397024f77f470
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-11-12 07:41:23 +0000

    Providing a direct URL to embedded model for downloading

--- install/1_download_embedding.sh
+++ install/1_download_embedding.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Downloading embedding.
-pushd .
-cd ../data/w2v && ./download.sh
-popd
+data=../data
+mkdir -p $data
+curl http://rusvectores.org/static/models/rusvectores2/news_mystem_skipgram_1000_20_2015.bin.gz -o "$data/news_rusvectores2.bin.gz"
