c187e995f64a8c196b84614ba43486af0b5eeeca
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-12-05 21:09:15 +0300

    erc-data dependency has been removed

--- install/0_code.sh
+++ install/0_code.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# 1. install core
 git clone -b 0.20.5-rc git@github.com:nicolay-r/AREkit.git ../arekit
-# 2. install data
-git clone -b 0.20.5 git@bitbucket.org:nicolay_r/entity-relation-data.git ../data 
-
