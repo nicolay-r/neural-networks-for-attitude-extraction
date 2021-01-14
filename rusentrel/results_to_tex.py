9f6473b56c4fe91bfaf3dd096f2e2419fc5ad979
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-01-14 15:58:26 +0000

    providing sorting, '' instead of na

--- rusentrel/results_to_tex.py
+++ rusentrel/results_to_tex.py
@@ -38,8 +38,9 @@ class ResultsTable:
         self.__output_dir = output_dir
 
     def save(self):
+        self.__df = self.__df.sort_values(by=[self.MODEL_NAME_COL])
         print self.__df
-        self.__df.to_latex("results.tex")
+        self.__df.to_latex("results.tex", na_rep='', index=False)
 
     @staticmethod
     def __parse_result(filepath):
