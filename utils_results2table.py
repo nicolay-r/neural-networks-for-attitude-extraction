5f8519a708475d9db9fbaea7b3b3561e3deffe60
Nicolay Rusnachenko <kolyarus@yandex.ru>
2021-01-16 12:39:29 +0300

    Refactoring. Providing FineTunned results capturing.

--- utils_results2table.py
+++ utils_results2table.py
@@ -11,18 +11,18 @@ sys.path.append('../')
 from arekit.common.experiment.folding.types import FoldingType
 from arekit.contrib.networks.enum_input_types import ModelInputType
 from arekit.contrib.networks.enum_name_types import ModelNames
-from arekit.contrib.source.ruattitudes.io_utils import RuAttitudesVersions
+from arekit.contrib.source.ruattitudes.io_utils import RuAttitudesVersions, RuAttitudesVersionsService
 from arekit.contrib.source.rusentrel.io_utils import RuSentRelVersions
 
 
-class ResultsTable:
+class ResultsTable(object):
 
     MODEL_NAME_COL = u'model_name'
     DS_TYPE_COL = u'ds_type'
 
     sl_template = u"rsr-{rsr_version}-{folding_str}-balanced-tpc50_{labels_count}l"
     sl_ds_template = u"rsr-{rsr_version}-ra-{ra_version}-{folding_str}-balanced-tpc50_{labels_count}l"
-    model_name_template = u"{folding_str}_{input_type}_{model_name}"
+    __model_name_template = u"{folding_str}_{input_type}_{model_name}"
 
     def __init__(self, input_type, output_dir):
         assert(isinstance(input_type, unicode))
@@ -40,10 +40,12 @@ class ResultsTable:
         self.__input_type = input_type
         self.__output_dir = output_dir
 
+    def _create_output_basename(self):
+        return "results-{input_type}".format(input_type=self.__input_type)
+
     def save(self):
-        self.__df.to_latex("results-{input_type}.tex".format(input_type=self.__input_type),
-                           na_rep='',
-                           index=False)
+        basename = self._create_output_basename()
+        self.__df.to_latex(basename + '.tex', na_rep='', index=False)
 
     @staticmethod
     def __parse_result(filepath):
@@ -82,11 +84,17 @@ class ResultsTable:
             assert(col_name in self.__df.columns)
             self.__df.set_value(row_ind, col_name, cv_value)
 
-    def __for_experiment(self, model_name, folding_type, experiment_dir, exp_type_name, labels_count):
+    def _create_model_dir(self, folding_str, model_str, exp_type_name):
+        return self.__model_name_template.format(folding_str=folding_str,
+                                                 input_type=self.__input_type,
+                                                 model_name=model_str)
+
+    def _for_experiment(self, model_name, folding_type, experiment_dir, ra_type, labels_count):
         assert(isinstance(model_name, ModelNames))
         assert(isinstance(folding_type, FoldingType))
-        assert(isinstance(exp_type_name, unicode))
+        assert(isinstance(ra_type, RuAttitudesVersions) or ra_type is None)
 
+        exp_type_name = ra_type.value if isinstance(ra_type, RuAttitudesVersions) else u'-'
         folding_str = u"cv" if folding_type == FoldingType.CrossValidation else u"fx"
         results_filepath = u"log/cb_eval_avg_test.log"
         model_str = model_name.value
@@ -95,9 +103,9 @@ class ResultsTable:
         # This allows us to combine neut with non-neut (for 2-scale).
         ds_col_type = exp_type_name.replace(u'_neut', '')
 
-        model_dir = self.model_name_template.format(folding_str=folding_str,
-                                                    input_type=self.__input_type,
-                                                    model_name=model_str)
+        model_dir = self._create_model_dir(folding_str=folding_str,
+                                           model_str=model_str,
+                                           exp_type_name=exp_type_name)
 
         # if the latter results are not presented
         # then we just reject the related line from the results.
@@ -122,14 +130,14 @@ class ResultsTable:
         # parsing results in order to organize the result table.
         it_results, avg_res = self.__parse_result(target_file)
 
-        # saving resutls.
+        # saving results.
         self.__save_results(it_results=it_results,
                             avg_res=avg_res,
                             folding_type=folding_type,
                             row_ind=row_ind,
                             labels_count=labels_count)
 
-    def __for_model(self, model_name, folding_type, labels_count, ra_version):
+    def register(self, model_name, folding_type, labels_count, ra_version):
         assert(isinstance(model_name, ModelNames))
         assert(isinstance(folding_type, FoldingType))
         assert(isinstance(ra_version, RuAttitudesVersions) or ra_version is None)
@@ -140,46 +148,106 @@ class ResultsTable:
 
         if ra_version is None:
             # Using supervised learning only
-            exp_type_name = u'-'
+            ra_type = None
             exp_dir = self.sl_template.format(rsr_version=rsr_version.value,
                                               folding_str=folding_str,
                                               labels_count=str(labels_count))
         else:
             # Using distant supervision in combination with supervised learning.
-            exp_type_name = ra_version.value
+            ra_type = ra_version
             exp_dir = self.sl_ds_template.format(rsr_version=rsr_version.value,
                                                  ra_version=ra_version.value,
                                                  folding_str=folding_str,
                                                  labels_count=str(labels_count))
 
-        self.__for_experiment(model_name=model_name,
-                              folding_type=folding_type,
-                              exp_type_name=exp_type_name,
-                              labels_count=labels_count,
-                              experiment_dir=exp_dir)
+        self._for_experiment(model_name=model_name,
+                             folding_type=folding_type,
+                             ra_type=ra_type,
+                             labels_count=labels_count,
+                             experiment_dir=exp_dir)
 
-    def fill(self):
-        for model_name in ModelNames:
-            for folding_type in FoldingType:
-                # for 3-scale
-                for ra_version in [RuAttitudesVersions.V20LargeNeut,
-                                   RuAttitudesVersions.V20BaseNeut,
-                                   RuAttitudesVersions.V12,
-                                   None]:
-                    self.__for_model(model_name=model_name,
-                                     folding_type=folding_type,
-                                     labels_count=3,
-                                     ra_version=ra_version)
 
-                # for 2-scale
-                for ra_version in [RuAttitudesVersions.V20Large,
-                                   RuAttitudesVersions.V20Base,
-                                   RuAttitudesVersions.V12,
-                                   None]:
-                    self.__for_model(model_name=model_name,
-                                     folding_type=folding_type,
-                                     labels_count=2,
-                                     ra_version=ra_version)
+class FineTunedResultsProvider(ResultsTable):
+
+    __fine_tuned_suffix = u"{model_name}-ft-{model_tag}"
+
+    __tags = {
+        RuAttitudesVersions.V12: u'ra12',
+        RuAttitudesVersions.V20Base: u'ra20b',
+        RuAttitudesVersions.V20BaseNeut: u'ra20bn',
+        RuAttitudesVersions.V20Large: u'ra20l',
+        RuAttitudesVersions.V20LargeNeut: u'ra20ln'
+    }
+
+    def _create_output_basename(self):
+        base_name = super(FineTunedResultsProvider, self)._create_output_basename()
+        return base_name + u'-ft'
+
+    @staticmethod
+    def __model_tag_from_ra_version(ra_version):
+        assert(isinstance(ra_version, RuAttitudesVersions))
+        return FineTunedResultsProvider.__tags[ra_version]
+
+    def _create_model_dir(self, folding_str, model_str, exp_type_name):
+        origin_name = super(FineTunedResultsProvider, self)._create_model_dir(
+            folding_str=folding_str,
+            model_str=model_str,
+            exp_type_name=exp_type_name)
+
+        ra_version = RuAttitudesVersionsService.find_by_name(exp_type_name)
+        updated_name = self.__fine_tuned_suffix.format(model_name=origin_name,
+                                                       model_tag=self.__model_tag_from_ra_version(ra_version))
+
+        return updated_name
+
+    def _for_experiment(self, model_name, folding_type, experiment_dir, ra_type, labels_count):
+        assert(ra_type is None)
+
+        # For every tag key we gathering results
+        # within a single experiment dir.
+        for ra_version in self.__tags.iterkeys():
+            super(FineTunedResultsProvider, self)._for_experiment(
+                model_name=model_name,
+                folding_type=folding_type,
+                experiment_dir=experiment_dir,
+                ra_type=ra_version,
+                labels_count=labels_count)
+
+
+def fill_single(res):
+    assert(isinstance(res, ResultsTable))
+    for model_name in ModelNames:
+        for folding_type in FoldingType:
+            # for 3-scale
+            for ra_version in [RuAttitudesVersions.V20LargeNeut,
+                               RuAttitudesVersions.V20BaseNeut,
+                               RuAttitudesVersions.V12,
+                               None]:
+                res.register(model_name=model_name,
+                             folding_type=folding_type,
+                             labels_count=3,
+                             ra_version=ra_version)
+
+            # for 2-scale
+            for ra_version in [RuAttitudesVersions.V20Large,
+                               RuAttitudesVersions.V20Base,
+                               RuAttitudesVersions.V12,
+                               None]:
+                res.register(model_name=model_name,
+                             folding_type=folding_type,
+                             labels_count=2,
+                             ra_version=ra_version)
+
+
+def fill_finetunned(res):
+    assert(isinstance(res, FineTunedResultsProvider))
+    for model_name in ModelNames:
+        for folding_type in FoldingType:
+            for labels in [2, 3]:
+                res.register(model_name=model_name,
+                             folding_type=folding_type,
+                             labels_count=labels,
+                             ra_version=None)
 
 
 if __name__ == "__main__":
@@ -203,13 +271,30 @@ if __name__ == "__main__":
                                  ModelInputType.MultiInstanceWithSelfAttention.value],
                         help='input type format')
 
+    parser.add_argument('--training-type',
+                        dest='training_type',
+                        type=unicode,
+                        nargs=1,
+                        default=u'single',
+                        choices=[u'single', u'ft'],
+                        help='Training format used for results gathering')
+
     args = parser.parse_args()
 
-    # fp = "output/rsr-v1_1-cv3-balanced-tpc50_3l/cv_ctx_cnn/log/cb_eval_avg_test.log"
-    # iters, last = __parse_and(filepath=fp)
-    # print iters, last
+    training_type = args.training_type[0]
 
-    rt = ResultsTable(output_dir=args.output_dir,
-                      input_type=args.input_type)
-    rt.fill()
+    rt = None
+    if training_type == u'single':
+        # Single training format.
+        rt = ResultsTable(output_dir=args.output_dir,
+                          input_type=args.input_type)
+        fill_single(rt)
+    elif training_type == u'ft':
+        # Fine-tuned results format.
+        rt = FineTunedResultsProvider(output_dir=args.output_dir,
+                                      input_type=args.input_type)
+        fill_finetunned(rt)
+    else:
+        raise NotImplementedError()
+
     rt.save()
